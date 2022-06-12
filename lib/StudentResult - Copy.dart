cb04a76f57320242102131a1c23e66648de6a1fb
ejomo <ejlalmohammed3@gmail.com>
2022-06-12 11:41:50 +0300

    add student result details page

---
+++ lib/StudentResult - Copy.dart
@@ -0,0 +1,170 @@
+﻿import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({Key key}) : super(key: key);
+
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: FlutterFlowTheme.of(context).primaryColor,
+        automaticallyImplyLeading: false,
+        leading: Form(
+          key: formKey,
+          autovalidateMode: AutovalidateMode.disabled,
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 24,
+            ),
+          ),
+        ),
+        title: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+          child: Text(
+            'student name',
+            style: FlutterFlowTheme.of(context).title1.override(
+                  fontFamily: 'Poppins',
+                  fontSize: 20,
+                ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+            child: Container(
+              width: 120,
+              height: 120,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              child: Image.network(
+                'https://picsum.photos/seed/172/600',
+              ),
+            ),
+          ),
+        ],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0.93, -0.99),
+                child: Card(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  color: Color(0xFFF5F5F5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Icon(
+                                  Icons.architecture_sharp,
+                                  color: Colors.black,
+                                  size: 24,
+                                ),
+                                Text(
+                                  'math',
+                                  style: FlutterFlowTheme.of(context).bodyText1,
+                                ),
+                                CircularPercentIndicator(
+                                  percent: 0.9,
+                                  radius: 25,
+                                  lineWidth: 10,
+                                  animation: true,
+                                  progressColor:
+                                      FlutterFlowTheme.of(context).primaryColor,
+                                  backgroundColor: Color(0xFFF1F4F8),
+                                  center: Text(
+                                    '90%',
+                                    style: FlutterFlowTheme.of(context)
+                                        .bodyText1
+                                        .override(
+                                          fontFamily: 'Poppins',
+                                          color: FlutterFlowTheme.of(context)
+                                              .primaryColor,
+                                        ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Icon(
+                              Icons.school_outlined,
+                              color: Colors.black,
+                              size: 24,
+                            ),
+                            Text(
+                              'physical',
+                              style: FlutterFlowTheme.of(context).bodyText1,
+                            ),
+                            CircularPercentIndicator(
+                              percent: 0.6,
+                              radius: 25,
+                              lineWidth: 10,
+                              animation: true,
+                              progressColor:
+                                  FlutterFlowTheme.of(context).primaryColor,
+                              backgroundColor: Color(0xFFF1F4F8),
+                              center: Text(
+                                '60%',
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyText1
+                                    .override(
+                                      fontFamily: 'Poppins',
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryColor,
+                                    ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
